243de81ee9d9975223c5c3771aa36656f946a22e
Lohit Kumar Biswas <lohit.kumar.biswas@outlook.com>
2023-08-04 00:39:31 +0530

    first commit

--- venv/learning python/file_detection.py
+++ venv/learning python/file_detection.py
@@ -4,5 +4,9 @@ path="C:\\Users\\ADMIN\\PycharmProjects\\currency_converter_chatbot"
 
 if os.path.exists(path):
     print("File is founded")
+    if os.path.isfile(path):
+        print("That is a file")
+    elif os.path.isdir(path):
+        print("That is a directory")
 else:
     print("File isn't found")
